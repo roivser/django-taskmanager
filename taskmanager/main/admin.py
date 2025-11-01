91d8127e230ecb5fb5b2d581d19b185dc2b617bf
Ivan <sir239@yandex.ru>
2025-11-01 12:03:05 +0300

    Create task model and add it to admin panel

--- taskmanager/main/admin.py
+++ taskmanager/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'status')
+    list_filter = ('status',)
